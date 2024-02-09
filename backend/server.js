45c2615f20db9035956e53caeaf56b645dd52459
Mihir Bhatkar <mihirbhatkar87@gmail.com>
2024-02-09 23:01:57 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -29,17 +29,7 @@ app.use(cookieParser());
 
 app.use("/api", routes);
 
-if (process.env.NODE_ENV === "production") {
-	const __dirname = path.resolve();
-
-	app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-	);
-} else {
-	app.get("/", (req, res) => res.send("server is running!"));
-}
+app.get("/", (req, res) => res.send("server is running!"));
 
 app.use(notFound);
 app.use(errorHandler);
