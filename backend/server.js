f87340bb3d1df4efff16c74baf4b00f018422eb7
Mihir Bhatkar <mihirbhatkar87@gmail.com>
2024-02-09 22:49:24 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,16 @@ connectDB();
 
 const app = express();
 
+import cors from "cors";
+
+app.use(
+	cors({
+		origin: ["https://stack-sense.vercel.app"],
+		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+		credentials: true,
+	})
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -20,19 +30,20 @@ app.use(cookieParser());
 app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
+	const __dirname = path.resolve();
 
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+	app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+	);
 } else {
-  app.get("/", (req, res) => res.send("server is running!"));
+	app.get("/", (req, res) => res.send("server is running!"));
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, "192.168.29.177", () =>
+	console.log(`Server started on port ${port}`)
+);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "https://stack-sense-api.vercel.app/",
+				changeOrigin: true,
+			},
+		},
+	},
 });
