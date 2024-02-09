824a4d032793345198edf3c3e756494ebd9336e2
Mihir Bhatkar <mihirbhatkar87@gmail.com>
2024-02-10 00:50:08 +0530

    vercel

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
 	res.cookie("jwt", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV !== "development", // because we only want this in production environment
-		sameSite: "strict", // prevents csrf attacks
+		sameSite: "none", // prevents csrf attacks
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 };
